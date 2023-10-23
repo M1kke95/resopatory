b431bf475658a9cd1f8602138c4096e1e0068473
M1kke95 <m1kke95@hotmail.com>
2023-10-23 21:26:53 +0200

    asd
    asd

--- src/test/java/testisLeapYearTest.java
+++ src/test/java/testisLeapYearTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.example.IsLeapYear;
 
-public class testisLeapYear {
+public class testisLeapYearTest {
 
     @Test
     public void test_isLeapYear(){

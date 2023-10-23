02ebc38f90e396c5a640036897a3041c151f0f66
M1kke95 <m1kke95@hotmail.com>
2023-10-23 21:06:26 +0200

    asd
    asd

--- src/test/java/test_isLeapYear.java
+++ src/test/java/test_isLeapYear.java
@@ -1,6 +1,6 @@
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.example.IsLeapYear;
